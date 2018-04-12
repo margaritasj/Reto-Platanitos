bee1500b2ad14186dce5da8b3b28e5e927457ce3
aurorex <selva_beat@hotmail.com>
2018-04-12 04:07:29 -0500

    agregando slider

--- js/subirFoto.js
+++ js/subirFoto.js
@@ -9,14 +9,40 @@
           reader.onload = (function(theFile) {
             return function(e) {
         // Creamos la imagen.
-              document.getElementById("list").innerHTML = ['<img class="img-output" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+              document.getElementById("list").innerHTML = ['<img class="img-output" src="', e.target.result,'" title="', escape(theFile.name), '" />'].join('');
             };
           })(f);
           reader.readAsDataURL(f);
         }
        }
        document.getElementById('files').addEventListener('change', archivo, false);
+      // SLIDER 
+  $('#slider').slick({
+  // normal options...
+    infinite: false,
+    // the magic
+    responsive: [{
     
+        breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            infinite: true
+          }
+      }, 
+      {
+        breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+          // dots: true
+          }
+  
+        }, 
+      {
+        breakpoint: 300,
+      // settings: "unslick" // destroys slick
+    }]
+  });
+
 
 })()  
     
