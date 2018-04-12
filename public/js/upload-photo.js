e2f70317604ac3df5ed7c0623dcd6012e3b1abaf
MelynaPernia <mebipeos@outlook.com>
2018-04-12 03:56:35 -0500

    Añadiendo la base de datos con firebase

--- public/js/upload-photo.js
+++ public/js/upload-photo.js
@@ -1,48 +1,69 @@
-(function(){
-    function archivo(evt) {
-        var files = evt.target.files;
-        for (var i = 0, f; f = files[i]; i++) {
-          if (!f.type.match('image.*')) {
-            continue;
-          }
-          var reader = new FileReader();
-          reader.onload = (function(theFile) {
-            return function(e) {
-              document.getElementById("modal-photo").style.display ='none';
-              document.getElementById("modal-aceptar").style.display ='block';
+(function () {
+  var config = {
+    apiKey: "AIzaSyCLoNdkDeyiO09zyiEpvVOubXExNSE-A5Y",
+    authDomain: "platanitos-e4b00.firebaseapp.com",
+    databaseURL: "https://platanitos-e4b00.firebaseio.com",
+    projectId: "platanitos-e4b00",
+    storageBucket: "platanitos-e4b00.appspot.com",
+    messagingSenderId: "879145201085"
+  };
+  firebase.initializeApp(config);
+  window.onload = initialize;
 
-              document.getElementById("aceptar").addEventListener("click", Aceptar);
+  var fileImage;
+  var imagesReference;
+  // imagenesFBRef
 
-              function Aceptar () {
-                
-                document.getElementById("list").innerHTML = ['<img class="img-output img-thumbnail" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
-              }
+  function initialize() {
+    fileImage = document.getElementById('files');
+    fileImage.addEventListener('change', uploadImage, false);
+    storageRef = firebase.storage().ref().child('imagenes');
+    imagesReference = firebase.database().ref().child('imagenes')
+    showImages();
+  }
 
-              document.getElementById("close").addEventListener("click", Cerrar);
-              document.getElementById("cancelar").addEventListener("click", Cerrar);
+  function uploadImage() {
+    var image = fileImage.files[0];
+    document.getElementById("modal-photo").style.display = 'none';
+    document.getElementById("modal-aceptar").style.display = 'block';
+    document.getElementById("aceptar").addEventListener("click", function () {
+      var uploadTask = storageRef.child(image.name).put(image)
+      uploadTask.on('state_changed',
+        function (snapshot) {},
+        function (error) {
+          alert('hubo un error');
+        },
+        function () {
+          var downloadURL = uploadTask.snapshot.downloadURL;
+          createNode(image.name, downloadURL);
+        });
+    });
 
-              function Cerrar () {
-                document.getElementById("modal-photo").style.display ='block';
-              document.getElementById("modal-aceptar").style.display ='none';
-                
-              }
 
-              document.getElementById("cancelar").addEventListener("click", Cerrar);
+  }
 
-             
-        // Creamos la imagen.
-              
-            };
-          })(f);
-          reader.readAsDataURL(f);
-        }
-       }
-       document.getElementById('files').addEventListener('change', archivo, false);
-    
+  function showImages() {
+    imagesReference.on('value', function (snapshot) {
+      var datos = snapshot.val();
+      // console.log(datos)
+      var result = "";
+      for (var key in datos) {
+        result += '<img class="img-output img-thumbnail"src="' + datos[key].url + '"/>';
+      }
+      document.getElementById('list').innerHTML = result;
+    })
 
-})()  
-    
-    
+  }
+
+
+  function createNode(nameImage, url) {
+    imagesReference.push({
+      nombre: nameImage,
+      url: url
+    });
+
+  }
+})()
 
 
 
